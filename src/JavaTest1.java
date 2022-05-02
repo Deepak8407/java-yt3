a8b760cd8a93df5f37ce0b54c4bf8f119b4abcda
deepakyadav8407 <101785656+deepakyadav8407@users.noreply.github.com>
2022-05-02 21:59:13 +0530

    added one line of code

--- src/JavaTest1.java
+++ src/JavaTest1.java
@@ -4,6 +4,7 @@ public class JavaTest1 {
 	public static void main(String[] args) {
 		System.out.println("Deepak Tukaram Yadav");
 		System.out.println("At.Masur");
+		System.out.println("Current Address: Pune");
 		
 		
 	}
